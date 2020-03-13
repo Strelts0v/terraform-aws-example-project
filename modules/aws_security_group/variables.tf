a0a1e228134c6e6baa76593501179c78ec200076
hleb.straltsou <hleb.straltsou@itechart-group.com>
2020-03-13 19:17:50 +0300

    Add AWS Security Group Module

---
+++ modules/aws_security_group/variables.tf
@@ -0,0 +1,39 @@
+variable "name" {
+  type    = string
+  default = "Web Server Security Group"
+}
+
+variable "vpc_id" {
+  type    = string
+  default = ""
+}
+
+variable "allowed_ports" {
+  description = "List of ports to open on server"
+  type        = list
+  default     = ["80", "443", "22", "8080"]
+}
+
+variable "ports_protocol" {
+  type    = string
+  default = "tcp"
+}
+
+variable "ingress_cidr_blocks" {
+  type    = list
+  default = ["0.0.0.0/0"]
+}
+
+variable "egress_cidr_blocks" {
+  type    = list
+  default = ["0.0.0.0/0"]
+}
+
+variable "tags" {
+  type = map
+  default = {
+    Owner       = "Hleb Straltsou"
+    Project     = "Example Project"
+    Environment = "dev"
+  }
+}
